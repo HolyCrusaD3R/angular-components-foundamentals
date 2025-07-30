5467aed38dbde53542fcc2d17718933d34932dc1
Luka Rekhviashvili <rekhviashvili.luka125@gmail.com>
2025-07-30 20:35:07 +0400

    signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,46 +3,47 @@ import { Component, signal } from '@angular/core';
 
 import { Human } from './human/human';
 import { Task } from './task/task';
+import { Counter } from './counter/counter';
+
 @Component({
   selector: 'app-root',
-  imports: [Task],
+  imports: [Counter],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('components');
   // cities = ['Tbilisi', 'New York'];
   // defaultCity = 'Tbilisi';
-  tasks = [
-    {
-      description: 'Buy Groceries',
-      priority: 'High',
-      id: 0,
-    },
-    {
-      description: 'Buy a Bicycle',
-      priority: 'High',
-      id: 1,
-    },
-    {
-      description: 'Fix the Computer',
-      priority: 'Low',
-      id: 2,
-    },
-    {
-      description: 'Attend a Party',
-      priority: 'High',
-      id: 3,
-    },
-    {
-      description: 'Study for Exams',
-      priority: 'Low',
-      id: 4,
-    },
-    {
-      description: 'Feed the Dog',
-      priority: 'High',
-      id: 5,
-    },
-  ];
+  // tasks = [
+  //   {
+  //     description: 'Buy Groceries',
+  //     priority: 'High',
+  //     id: 0,
+  //   },
+  //   {
+  //     description: 'Buy a Bicycle',
+  //     priority: 'High',
+  //     id: 1,
+  //   },
+  //   {
+  //     description: 'Fix the Computer',
+  //     priority: 'Low',
+  //     id: 2,
+  //   },
+  //   {
+  //     description: 'Attend a Party',
+  //     priority: 'High',
+  //     id: 3,
+  //   },
+  //   {
+  //     description: 'Study for Exams',
+  //     priority: 'Low',
+  //     id: 4,
+  //   },
+  //   {
+  //     description: 'Feed the Dog',
+  //     priority: 'High',
+  //     id: 5,
+  //   },
+  // ];
 }
